a7a14b9e4f8a83538f25a860163c330536c8996f
sam <83267739+sammed-code@users.noreply.github.com>
2024-01-23 10:04:28 +0000

    2

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # 11023-03
 
 
-NodeJS
+## NodeJS
 
 npm -> node package manager
 npm init
 
 node server.js
 npm start / npm run dev(nodemon)
+
+### HTTP Methods:
+####    -> GET: Getting data from server
+####    -> POST: Sending data to server
+####    -> DELETE: Deleting data from server
+####    -> PATCH: Updating certain fields
+####    -> PUT: Full update
--- server.js
+++ server.js
@@ -1,11 +1,68 @@
+const { error } = require("console");
 const http = require("http");
 
 const port = 8081;
 
+const toDoList = ["Hey Everyone", "Hope U All", "Are Doing", "Awesome At Your Work"];
+
 http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.write("<h4>Hello World!</h4>");
-    res.write("<h6>Hey we got our server...</h6>");
+    const {method, url} = req;
+
+    if (url === "/todos") {
+        if (method === "GET") {
+            res.writeHead(200, {"Content-Type": "text/html"});
+            console.log("TODOS");
+            res.write(toDoList.toString());
+        }
+        else if (method === "POST") {
+            let body = "";
+            req.on('error', (err) => {
+                console.log(err);
+            }).on('data', (chunk) => {
+                body += chunk;
+                console.log("chunk: ", chunk);
+            }).on('end', () => {
+                body = JSON.parse(body);
+                console.log("data: ", body);
+                let newToDo = toDoList;
+                newToDo.push(body.item)
+            })
+        }
+        else if (method === "PUT") {
+            
+        }
+        else if (method === "DELETE") {
+            let body = "";
+            req.on('error', (err) => {
+                console.log(err);
+            }).on('data', (chunk) => {
+                body += chunk;
+            }).on('end', () => {
+                body = JSON.parse(body);
+                let delThis = body.item;
+
+                // for (let i = 0; i < toDoList.length; i++) {
+                //     if (toDoList[i] === delThis) {
+                //         toDoList.splice(i, 1);
+                //         break;
+                //     }
+                // }
+
+                toDoList.find((elem, i) => {
+                    if (elem === delThis) {
+                        toDoList.splice(i, 1);
+                    }
+                })
+            })
+        }
+        else {
+            res.writeHead(501);
+        }
+    }
+    else if (url === "/") {
+        
+    }
+
     res.end();
 }).listen(port, () => {
     console.log(`NodeJS server started running on port ${port}`);
