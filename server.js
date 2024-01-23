6c96ee9ee1913d21d17dc327ad5a2ff10e6cdece
sam <83267739+sammed-code@users.noreply.github.com>
2024-01-23 10:52:58 +0000

    4

--- server.js
+++ server.js
@@ -13,6 +13,41 @@ app.get("/todos", (req, res) => {
     res.status(200).send(toDoList);
 })
 
+app.post("/todos", (req, res) => {
+    let newToDo = req.body.item;
+    toDoList.push(newToDo);
+    res.status(201).send({
+        message: "The task was successfully added"
+    })
+})
+
+app.delete("/todos", (req, res) => {
+    let itemToDelete = req.body.item;
+
+    toDoList.find((e, i) => {
+        if (e === itemToDelete) {
+            toDoList.splice(i, 1);
+        }
+    });
+
+    res.status(204).send({
+        message: `Deleted item ${req.body.item}`
+    })
+})
+
+// Apart from above executed it executes remaining all
+app.all("/todos", (req, res) => {
+    res.status(501).send({
+        message: "Not yet implemented"
+    })
+})
+
+app.all("*", (req, res) => {
+    res.status(404).send({
+        message: "Wrong url"
+    })
+})
+
 app.listen(port, () => {
     console.log(`NodeJS with Express started successfully on port ${port}`);
 })
